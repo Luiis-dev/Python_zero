2636cc8c24da7412f54fadaa85cd4448f70c3428
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-08-23 13:33:23 -0300

    conteúdos sobre módulos da aula 08 de python

---
+++ PYTHON_MUNDO_01/Utilizando Módulos/aula.py
@@ -0,0 +1,18 @@
+# modulos são arquivos .py que contém funções, classes
+# e variáveis que podem ser reutilizadas em outros programas.
+
+# import bebidas # todas as bebidas
+# from bebidas import refrigerante # apenas refrigerante
+
+# biblioteca que já vem com o python math
+# math
+#     ceil() # arredonda para cima
+#     floor() # arredonda para baixo
+#     trunc() # arredonda para zero
+#     pow() # potenciação
+#     sqrt() # raiz quadrada
+#     factorial() # fatorial
+
+# import math # importando toda a biblioteca
+
+# from math import sqrt # importando apenas a função sqrt
