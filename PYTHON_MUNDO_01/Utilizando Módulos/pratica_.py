588915a544b0612557d09b7457499b5ab4b9730f
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-08-24 22:13:53 -0300

    forma do math

--- PYTHON_MUNDO_01/Utilizando Módulos/pratica_.py
+++ PYTHON_MUNDO_01/Utilizando Módulos/pratica_.py
@@ -1,1 +1,13 @@
-num = int
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print("A raiz de {} é igual a {}.".format(num, math.ceil(raiz)))
+
+# ceil() arredonda para cima
+
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print("A raiz de {} é igual a {:.2f}.".format(num, math.floor(raiz)))
+
+# floor() arredonda para baixo
