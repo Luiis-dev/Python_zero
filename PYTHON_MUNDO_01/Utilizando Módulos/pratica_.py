1be3d0a6ac966f95a7fdfe5448eae0112199c55c
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-08-24 22:17:52 -0300

    importando apenas as funções
    sqrt e floor

--- PYTHON_MUNDO_01/Utilizando Módulos/pratica_.py
+++ PYTHON_MUNDO_01/Utilizando Módulos/pratica_.py
@@ -3,6 +3,7 @@ num = int(input('Digite um número: '))
 raiz = math.sqrt(num)
 print("A raiz de {} é igual a {}.".format(num, math.ceil(raiz)))
 
+# import math = importando toda a biblioteca
 # ceil() arredonda para cima
 
 import math
@@ -11,3 +12,10 @@ raiz = math.sqrt(num)
 print("A raiz de {} é igual a {:.2f}.".format(num, math.floor(raiz)))
 
 # floor() arredonda para baixo
+
+from math import sqrt, floor
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print("A raiz de {} é igual a {:.2f}.".format(num, floor(raiz)))
+
+# from math import sqrt, floor = importando apenas as funções sqrt e floor
