94be8de77ad6cdbdc779f83f7dbe6bb7ba8ea1f3
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-08-24 22:24:14 -0300

    random
    biblioteca random aplicada

--- PYTHON_MUNDO_01/Utilizando Módulos/pratica_.py
+++ PYTHON_MUNDO_01/Utilizando Módulos/pratica_.py
@@ -19,3 +19,15 @@ raiz = sqrt(num)
 print("A raiz de {} é igual a {:.2f}.".format(num, floor(raiz)))
 
 # from math import sqrt, floor = importando apenas as funções sqrt e floor
+
+import random
+num = random.random()
+print(num)
+
+# random.random() = gera um número float aleatório entre 0 e 1
+
+import random
+num = random.randint(1, 10)
+print(num)
+
+# random.randint(1, 10) = gera um número inteiro aleatório entre 1 e 10
