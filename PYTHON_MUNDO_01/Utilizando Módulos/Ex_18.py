771b540be3777bf9154e8d972fff34aaf06b964a
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-09-06 16:16:15 -0300

    finalizado com a bibliote radians, sin, cos, tan

--- PYTHON_MUNDO_01/Utilizando Módulos/Ex_18.py
+++ PYTHON_MUNDO_01/Utilizando Módulos/Ex_18.py
@@ -21,3 +21,15 @@ cosseno = math.cos(math.radians(ângulo))
 print("O ângulo de {} tem o COSSENO de {:.2f}".format(ângulo, cosseno))
 tangente = math.tan(math.radians(ângulo))
 print("O ângulo de {} tem a TANGENTE de {:.2f}".format(ângulo, tangente))
+
+# Outro jeito de fazer
+
+from math import radians, sin, cos, tan
+ângulo = float(input("Digite um ângulo:  "))
+seno = sin(radians(ângulo))
+print("O ângulo de {} tem o SENO de {:.2f}".format(ângulo, seno))
+cosseno = cos(radians(ângulo))
+print("O ângulo de {} tem o COSSENO de {:.2f}".format(ângulo, cosseno))
+tangente = tan(radians(ângulo))
+print("O ângulo de {} tem o TANGENTE de {:.2f}".format(ângulo, tangente))
+
