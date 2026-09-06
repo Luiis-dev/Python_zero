43abf2e890e9d63e104e7902483cd698a81a44f7
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-09-06 13:10:54 -0300

    terminei

--- PYTHON_MUNDO_01/Utilizando Módulos/Ex_21.py
+++ PYTHON_MUNDO_01/Utilizando Módulos/Ex_21.py
@@ -3,3 +3,9 @@
 # faça um programa em python que abra 
 # e reproduza o áudio de um arquivo mp3
 
+import os
+
+pasta_script = os.path.dirname(os.path.abspath(__file__))
+caminho_mp3 = os.path.join(pasta_script, "Escondido Em Mim.mp3")
+
+os.startfile(caminho_mp3)
