786a41d7f887321593b7ad29988cfec6f3f0c119
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-09-06 16:42:08 -0300

    import random shuffle

--- PYTHON_MUNDO_01/Utilizando Módulos/Ex_20.py
+++ PYTHON_MUNDO_01/Utilizando Módulos/Ex_20.py
@@ -11,3 +11,15 @@ ordem_de_apresenta = alunos.split()
 shuffle(ordem_de_apresenta)
 print("A ordem de apresentação será:")
 print(ordem_de_apresenta)
+
+# Resposta do Guaanabara
+
+import random
+n1 = str(input("Primeiro aluno: "))
+n2 = str(input("Segundo aluno: "))
+n3 = str(input("Terceiro aluno: "))
+n4 = str(input("Quarto aluno: "))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print("A ordem de apresentação será ")
+print(lista)
