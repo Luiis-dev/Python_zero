e7d6c63252358c610c899891ebe1c9424015cff7
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-09-06 15:28:32 -0300

    outra forma sem utilizar a biblioteca

--- PYTHON_MUNDO_01/Utilizando Módulos/Ex_16.py
+++ PYTHON_MUNDO_01/Utilizando Módulos/Ex_16.py
@@ -13,3 +13,15 @@ print("O número digitado foi {} e a sua porcão inteira é {}".format(n1, numer
 import math
 num = float(input("Digite um número: "))
 print("O número digitado foi {} e a sua porção inteira é {}".format(num, math.trunc(num)))
+
+# Ou
+
+from math import trunc
+num = float(input("Digite um número: "))
+print("O número digitado foi {} e a sua porção inteira é {}".format(num, trunc(num)))
+
+# Outra forma de fazer sem utilizar a matemática
+
+num = float(input("Digite um número: "))
+print("O número digitado foi {} e a sua porção inteira é {}".format(num, int(num)))
+
