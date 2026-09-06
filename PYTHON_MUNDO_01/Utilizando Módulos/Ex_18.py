ed72f5d9e27a202cbda47e7b7b3db3bd98b58816
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-09-06 16:09:39 -0300

    Forma importando por completo

--- PYTHON_MUNDO_01/Utilizando Módulos/Ex_18.py
+++ PYTHON_MUNDO_01/Utilizando Módulos/Ex_18.py
@@ -10,3 +10,14 @@ sen = math.sin(math.radians(angulo))
 cos = math.cos(math.radians(angulo))
 tan = math.tan(math.radians(angulo))
 print("O ângulo digitado foi {} e o sen {}, cos {} e tan {}".format(angulo, sen, cos, tan))
+
+# Resposta do Guanabara
+
+import math
+ângulo = float(input("Digite um ângulo:  "))
+seno = math.sin(math.radians(ângulo))
+print("O ângulo de {} tem o SENO de {:.2f}".format(ângulo, seno))
+cosseno = math.cos(math.radians(ângulo))
+print("O ângulo de {} tem o COSSENO de {:.2f}".format(ângulo, cosseno))
+tangente = math.tan(math.radians(ângulo))
+print("O ângulo de {} tem a TANGENTE de {:.2f}".format(ângulo, tangente))
