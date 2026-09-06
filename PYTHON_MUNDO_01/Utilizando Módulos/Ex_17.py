058351978946aef709dddd40a9edd4d5abf36228
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-09-06 15:41:53 -0300

    somente a funcão

--- PYTHON_MUNDO_01/Utilizando Módulos/Ex_17.py
+++ PYTHON_MUNDO_01/Utilizando Módulos/Ex_17.py
@@ -25,4 +25,11 @@ ca = float(input("Comprimento do cateto adjacente: "))
 hi = math.hypot(co, ca)
 print("A hipotenusa vai medir {:.2f}".format(hi))
 
+# somente a função hypot() da biblioteca math
+
+from math import hypot
+co = float(input("Comprimento do cateto oposto: "))
+ca = float(input("Comprimento do cateto adjacente: "))
+hi = hypot(co, ca)
+print("A hipotenusa vai medir {:.2f}".format(hi))
 
