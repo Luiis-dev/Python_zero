17da808da5e884d46bd5f49138c0a60f174ca11f
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-09-15 20:13:14 -0300

    Questão

--- PYTHON_MUNDO_01/Manipulando Texto/Ex_26.py
+++ PYTHON_MUNDO_01/Manipulando Texto/Ex_26.py
@@ -1,1 +1,11 @@
  
+# Exercicio 26
+
+# Faça um programa que leia uma frase pelo teclado e mostre:
+
+# quantos vezes aparece a letra "A".
+
+# em que posição ela aparece a primeira vez.
+
+# em que posição ela aparece a última vez.
+
