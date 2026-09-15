bf4de2450ede7b53d2773afb8d78559d77a2aa61
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-09-15 20:16:00 -0300

    Questão

--- PYTHON_MUNDO_01/Manipulando Texto/Ex_27.py
+++ PYTHON_MUNDO_01/Manipulando Texto/Ex_27.py
@@ -1,1 +1,10 @@
  
+# Exercicio 27
+
+# faça um programa que leia o nome pessoa, mostrando em
+# seguida o primeiro e o última nome separadamente.
+
+# Ex: Ana Maria de Souza
+# Primeiro = Ana
+# última = Souza
+
