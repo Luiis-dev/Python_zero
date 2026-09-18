2f89f9cf9b009931d6df4f418c5b5795fdbe86c0
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-09-17 21:26:33 -0300

    completo
    funcionando perfeitamente para as 7 operações matemáticas
    
    vou fazer mais......

---
+++ PYTHON_MUNDO_01/Operadores Aritméticos/Praticar_por_fora.py
@@ -0,0 +1,19 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outr número: "))
+adicao = n1 + n2
+subtracacao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+resto_divisao = n1 % n2
+divisao_inteira = n1 // n2 
+potenciacao = n1 ** n2
+
+print(f"""
+Adiçao: {adicao}
+Subtração: {subtracacao}
+Multiplicação: {multiplicacao}
+Divisão: {divisao}
+Resto da divisão: {resto_divisao}
+Divisão inteira: {divisao_inteira}
+Potenciação: {potenciacao}
+""")
