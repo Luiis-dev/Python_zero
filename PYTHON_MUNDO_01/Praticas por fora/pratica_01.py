d912a2dd0541f98c055ba9c2949ac68b0ac89d63
Evton Luís <103472691+Luiis-dev@users.noreply.github.com>
2026-09-19 19:39:00 -0300

    Finalizado corretamente

--- PYTHON_MUNDO_01/Praticas por fora/pratica_01.py
+++ PYTHON_MUNDO_01/Praticas por fora/pratica_01.py
@@ -1,19 +1,24 @@
+# nivel 1 — Operadores básicos
+
+# Questão 1
+
+# Peça dois números ao usuário e mostre
+# o resultado das 4 operações básicas (+, -, *, /). 
+
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outr número: "))
 adicao = n1 + n2
 subtracacao = n1 - n2
 multiplicacao = n1 * n2
 divisao = n1 / n2
-resto_divisao = n1 % n2
-divisao_inteira = n1 // n2 
-potenciacao = n1 ** n2
 
 print(f"""
 Adiçao: {adicao}
 Subtração: {subtracacao}
 Multiplicação: {multiplicacao}
 Divisão: {divisao}
-Resto da divisão: {resto_divisao}
-Divisão inteira: {divisao_inteira}
-Potenciação: {potenciacao}
 """) 
+
+# Questão 2
+
+
